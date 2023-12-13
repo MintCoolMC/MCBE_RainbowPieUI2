4b68148963a44ab2a24e606a8cb5de146bcebe2c
Mintraspberry Cool <124319497+MintCoolMC@users.noreply.github.com>
2023-12-13 17:16:42 +0800

    update

--- zipmcproject.py
+++ zipmcproject.py
@@ -3,16 +3,20 @@ import os
 import zipfile
 import re
 
-# Get the current commit id
-commit_id = os.popen("git rev-parse --short HEAD").read().strip()
-
-with open("ui/_global_variables.json", "r+", encoding="utf-8") as f:
+def writeGlobalVarStr(key,value):
+  with open("ui/_global_variables.json", "r+", encoding="utf-8") as f:
     text = f.read()
-    pattern = r'\$rainbowpieui_commit_id": ".*?"'
-    text = re.sub(pattern, f'$rainbowpieui_commit_id": "{commit_id}"', text)
+    pattern = r'\${key}": ".*?"'
+    text = re.sub(pattern, f'${key}": "{value}"', text)
     f.seek(0)
     f.write(text)
     f.close()
+
+commit_id = os.popen("git rev-parse --short HEAD").read().strip()
+branch = os.popen("git rev-parse --abbrev-ref HEAD").read().strip()
+
+writeGlobalVarStr("rainbowpieui_commit_id",commit_id)
+writeGlobalVarStr("rainbowpieui_branch_name",branch)
 
 # Read the json file
 data = demjson.decode_file("ui/_global_variables.json")
