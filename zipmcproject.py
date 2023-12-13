9d6065e627784d8961bc7b4c4f528cb8169262e0
Mintraspberry Cool <124319497+MintCoolMC@users.noreply.github.com>
2023-12-13 15:11:18 +0800

    update

--- zipmcproject.py
+++ zipmcproject.py
@@ -8,9 +8,11 @@ data = demjson.decode_file("ui/_global_variables.json")
 # Get the values
 name = data.get("$mintui_pack_name").replace(" ","")
 version = data.get("$mintui_pack_version")
+branch = data.get("$rainbowpieui_branch_name")
+mcversion = data.get("$rainbowpieui_version_minecraft_target")
 
 # Create the zip file name
-zip_file_name = f"{name}_{version}.zip"
+zip_file_name = f"{name}-v{version}.{branch}_{mcversion}.zip"
 
 # Run the zip command
 os.system(f"zip -r \"{zip_file_name}\" . -x '.github/*' -x '.git/*' -x 'zipmcproject.py'")
