4b32397ee89ee20c66f610ff4ee3cd3e7c3203cf
Mintraspberry Cool <124319497+MintCoolMC@users.noreply.github.com>
2023-12-13 17:29:17 +0800

    Update zipmcproject.py

--- zipmcproject.py
+++ zipmcproject.py
@@ -7,8 +7,8 @@ def writeGlobalVarStr(key,value):
   with open("ui/_global_variables.json", "r+", encoding="utf-8") as f:
     text = f.read()
     pattern = r'"\$'+key+'": ".*?"'
-    print("writeVar"+pattern)
     text = re.sub(pattern, f'"$'+key+'": "'+value+'"', text)
+    print("[writeVar] "+text)
     f.seek(0)
     f.write(text)
     f.close()
