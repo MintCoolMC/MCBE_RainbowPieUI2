8d4c2dd5c19e54e2fb7491137dc87a03ec4a21b9
Mintraspberry Cool <124319497+MintCoolMC@users.noreply.github.com>
2023-12-13 18:24:44 +0800

    update

--- zipmcproject.py
+++ zipmcproject.py
@@ -12,26 +12,24 @@ def writeGlobalVarStr(key,value):
     f.seek(0)
     f.truncate(0)
     f.write(text)
-    # f.close()
 
 commit_id = os.popen("git rev-parse --short HEAD").read().strip()
 branch = os.popen("git rev-parse --abbrev-ref HEAD").read().strip()
 
 writeGlobalVarStr("rainbowpieui_commit_id",commit_id)
 writeGlobalVarStr("rainbowpieui_branch_name",branch)
-os.system(f"zip -r \"test.mcpack\" . -x '.github/*' -x '.git/*' -x 'zipmcproject.py'")
 
-# # Read the json file
-# data = demjson.decode_file("ui/_global_variables.json")
+# Read the json file
+data = demjson.decode_file("ui/_global_variables.json")
 
-# # Get the values
-# name = data.get("$mintui_pack_name").replace(" ","")
-# version = data.get("$mintui_pack_version")
-# branch = data.get("$rainbowpieui_branch_name")
-# mcversion = data.get("$rainbowpieui_version_minecraft_target")
+# Get the values
+name = data.get("$mintui_pack_name").replace(" ","")
+version = data.get("$mintui_pack_version")
+branch = data.get("$rainbowpieui_branch_name")
+mcversion = data.get("$rainbowpieui_version_minecraft_target")
 
-# # Create the zip file name
-# zip_file_name = f"{name}-v{version}.{branch}_{mcversion}.mcpack"
+# Create the zip file name
+zip_file_name = f"{name}-v{version}.{branch}_{mcversion}.mcpack"
 
-# # Run the zip command
-# os.system(f"zip -r \"{zip_file_name}\" . -x '.github/*' -x '.git/*' -x 'zipmcproject.py'")
+# Run the zip command
+os.system(f"zip -r \"{zip_file_name}\" . -x '.github/*' -x '.git/*' -x 'zipmcproject.py' -x 'README.md'")
