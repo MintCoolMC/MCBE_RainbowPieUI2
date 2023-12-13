09f3c5c63c0a94bda9551f25dfb7b51cd1c197ce
Mintraspberry Cool <124319497+MintCoolMC@users.noreply.github.com>
2023-12-13 15:05:09 +0800

    update

--- zipmcproject.py
+++ zipmcproject.py
@@ -12,8 +12,5 @@ version = data.get("$mintui_pack_version")
 # Create the zip file name
 zip_file_name = f"{name}_{version}.zip"
 
-# Zip the project files
-with zipfile.ZipFile(zip_file_name, "w") as z:
-  for file in os.listdir("."):
-    if file != "ui/_global_variables.json" and file != zip_file_name:
-      z.write(file)
+# Run the zip command
+os.system(f"zip -r {zip_file_name} . -x '.github/*' -x '.git/*' -x 'zipmcproject.py'")
