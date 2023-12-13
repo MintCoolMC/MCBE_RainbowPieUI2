c856d4539d1c8e3f974cad8b9b3d9b1f44e471ba
Mintraspberry Cool <124319497+MintCoolMC@users.noreply.github.com>
2023-12-13 15:07:30 +0800

    update

--- zipmcproject.py
+++ zipmcproject.py
@@ -6,11 +6,11 @@ import zipfile
 data = demjson.decode_file("ui/_global_variables.json")
 
 # Get the values
-name = data.get("$mintui_pack_name")
+name = data.get("$mintui_pack_name").replace(" ","")
 version = data.get("$mintui_pack_version")
 
 # Create the zip file name
 zip_file_name = f"{name}_{version}.zip"
 
 # Run the zip command
-os.system(f"zip -r {zip_file_name} . -x '.github/*' -x '.git/*' -x 'zipmcproject.py'")
+os.system(f"zip -r \"{zip_file_name}\" . -x '.github/*' -x '.git/*' -x 'zipmcproject.py'")
